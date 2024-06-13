40ea5db00bc9d0de31eac8d9bdd0769dcdbad85f
Abdul12majid <yisaabdulmajid@gmail.com>
2024-06-13 17:25:04 +0100

    Static-files and Routing settings

--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kanban_app',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+#STATIC_ROOT = BASE_DIR/'assets'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static/')
+]
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Adjust path if necessary
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
